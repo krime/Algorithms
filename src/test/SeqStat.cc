359391b67e87eb16a4235275d3d73fce00ccc2e0
krime <krimelam@gmail.com>
2013-04-02 22:37:14 +0800

    Undate Testing

--- src/test/SeqStat.cc
+++ src/test/SeqStat.cc
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <vector>
+
 #include <cctype>
 #include <cstdlib>
+#include <cstring>
 #include <ctime>
 
 using namespace std;
@@ -13,7 +15,7 @@ public:
   typedef int (*compare)(void*,void*);
 
   SortSeq(void):v(NULL){}
-  SortSeq(T arr[]):v(arr){}
+  SortSeq(T arr[],size_type n):v(arr,arr+n){}
   SortSeq(vector<T> vec):v(vec){}
   SortSeq(const SortSeq& s):v(s.v){}
   SortSeq& operator=(SortSeq& s) {v=s.v;return *this;}
@@ -25,6 +27,8 @@ public:
   size_type MergeStat(vector<T>,size_type,size_type,size_type,compare);
   // Permutation
   void Shuffle(void);
+  // Display
+  void Display(void) {for (size_type i=0;i<v.size();i++)cout<<' '<<v[i];cout<<endl;}
 private:
   void swap(T&r,T&s) {T t=r;r=s;s=t;}
 
@@ -68,7 +72,15 @@ template<typename T> void SortSeq<T>::Shuffle(void) {
   size_type n=v.size();
   for (size_type i=0;i<n;i++) {
     size_type r=::rand()%(n-i)+i;
-    swap(&v[i],&v[r]);
+    swap(v[i],v[r]);
   }
 }
 
+int main(void) {
+  unsigned int num[128]={0};
+  for (unsigned int i=0;i<128;i++) num[i]=i;
+  SortSeq<unsigned int> seq(num,128);
+  seq.Display();
+  seq.Shuffle();
+  seq.Display();
+}
