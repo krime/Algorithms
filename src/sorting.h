6dda1d808fef3d88ef3c42629e61e5fdbfd61c20
krime <krimelam@gmail.com>
2013-04-07 19:50:18 +0800

    Fix Bugs

--- src/sorting.h
+++ src/sorting.h
@@ -47,36 +47,14 @@ namespace sort {
   void      HeapSort(vector<T>&,size_type,size_type,compare);
 
   template<typename T>
-  void InsertionSort(vector<T>&v,compare fc) {InsertionSort(v,0,v.size(),fc);}
-  template<typename T>
-  void SelectionSort(vector<T>&v,compare fc) {SelectionSort(v,0,v.size(),fc);}
-  template<typename T>
-  void    BubbleSort(vector<T>&v,compare fc) {   BubbleSort(v,0,v.size(),fc);}
-  template<typename T>
-  void     MergeSort(vector<T>&v,compare fc) {    MergeSort(v,0,v.size(),fc);}
-  template<typename T>
-  void     QuickSort(vector<T>&v,compare fc) {    QuickSort(v,0,v.size(),fc);}
-  template<typename T>
-  void      HeapSort(vector<T>&v,compare fc) {     HeapSort(v,0,v.size(),fc);}
-
-  template<typename T>
   void Merge(vector<T>&,size_type,size_type,size_type,compare);
   template<typename T>
   size_type Partition(vector<T>&,size_type,size_type,compare);
   template<typename T>
   void Heapify(vector<T>&,size_type,size_type,size_type,compare);
-  /*
-  //private:
-  Sort(){}
-  Sort(const Sort&){}
-  Sort& operator=(Sort&){}
-  const Sort& operator=(const Sort&){}
-  ~Sort(){}
-  */
+
   template<typename T>
   void swap(T&r,T&s) {T t=r;r=s;s=t;}
-  // Number of Reversed Pairs
-  // size_type rp;
 }
 
 namespace sort {
