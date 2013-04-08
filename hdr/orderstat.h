befdb9d0ed83b89e2c5713f9bc64b6dfc55e4386
krime <krimelam@gmail.com>
2013-04-08 19:16:41 +0800

    Order Statistics

--- hdr/orderstat.h
+++ hdr/orderstat.h
@@ -14,6 +14,22 @@ namespace stat {
   
   template<typename T>
   void swap(T&r,T&s) {T t=r;r=s;s=t;}
+
+  template<typename T>
+  size_type RandomPartition(vector<T>&v,size_type p,size_type r,compare fc) {
+    ::srand(::time(NULL));
+    size_type n=::rand()%(r-p)+p;
+    
+    if (n!=r-1) swap(v[n],v[r-1]);
+    size_type i=p-1;
+    T pivot=v[r-1];
+    for (size_type j=p;j<r-1;j++)
+      if (fc((void*)&v[j],(void*)&pivot)<0)
+        if (++i!=j)
+          swap(v[i],v[j]);
+    swap(v[i+1],v[r-1]);
+    return i+1;
+  }
   
   template<typename T>
   size_type RandomSelect(vector<T>&v,size_type p,size_type r,size_type k,compare fc) {
@@ -28,38 +44,7 @@ namespace stat {
     else
       return RandomSelect(v,q,r,k-i-1,fc);
   }
-
-  template<typename T>
-  size_type RandomPartition(vector<T>&v,size_type p,size_type r,compare fc) {
-    ::srand(::time(NULL));
-    size_type n=::rand()%(r-p)+p;
-    swap(v[n],v[r-1]);
-    size_type i=p-1;
-    T pivot=v[r-1];
-    for (size_type j=p;j<r-1;j++)
-      if (fc((void*)&v[j],(void*)&pivot)<0)
-        if (++i!=j)
-          swap(v[i],v[j]);
-    swap(v[i+1],v[r-1]);
-    return i+1;
-  }
   
-  template<typename T>
-  size_type OrderStat(vector<T>&v,compare fc) {
-    return OrderStat(v,0,v.size(),fc);
-  }
-  
-  template<typename T>
-  size_type OrderStat(vector<T>&v,size_type p,size_type r,compare fc) {
-    if (p<r-1&&r) {
-      size_type q=(r-p)/2+p;
-      size_type r1=OrderStat(v,p,q,fc);
-      size_type r2=OrderStat(v,q,r,fc);
-      size_type r3=MergeStat(v,p,q,r,fc);
-      return r1+r2+r3;
-    } else return 0;
-  }
-
   template<typename T>
   size_type MergeStat(vector<T>&v,size_type p,size_type q,size_type r,compare fc) {
     vector<T> L(v.begin()+p,v.begin()+q);
@@ -84,6 +69,22 @@ namespace stat {
     return rp;
   }
 
+  template<typename T>
+  size_type OrderStat(vector<T>&v,size_type p,size_type r,compare fc) {
+    if (p<r-1&&r) {
+      size_type q=(r-p)/2+p;
+      size_type r1=OrderStat(v,p,q,fc);
+      size_type r2=OrderStat(v,q,r,fc);
+      size_type r3=MergeStat(v,p,q,r,fc);
+      return r1+r2+r3;
+    } else return 0;
+  }
+
+  template<typename T>
+  size_type OrderStat(vector<T>&v,compare fc) {
+    return OrderStat(v,0,v.size(),fc);
+  }
+  
   template<typename T>
   size_type BruteForceStat(vector<T>&v,size_type p,size_type r,compare fc) {
     size_type s=0;
