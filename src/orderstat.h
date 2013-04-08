59eff648c90ebcf48675a07df71e5552a0189a84
krime <krimelam@gmail.com>
2013-04-08 02:53:51 +0000

    Bugs Fix

--- src/orderstat.h
+++ src/orderstat.h
@@ -10,7 +10,7 @@ using namespace std;
 
 namespace stat {
   typedef unsigned long size_type;
-  typedef long (*compare)(void*,void*);
+  typedef int (*compare)(void*,void*);
   
   template<typename T>
   void swap(T&r,T&s) {T t=r;r=s;s=t;}
@@ -43,25 +43,25 @@ namespace stat {
     swap(v[i+1],v[r-1]);
     return i+1;
   }
-
+  
   template<typename T>
-  size_type OrderStat(compare fc) {
-    return OrderStat(0,v.size(),fc);
+  size_type OrderStat(vector<T>&v,compare fc) {
+    return OrderStat(v,0,v.size(),fc);
   }
-
-  template<typedef T>
-  size_type OrderStat(size_type p,size_type r,compare fc) {
+  
+  template<typename T>
+  size_type OrderStat(vector<T>&v,size_type p,size_type r,compare fc) {
     if (p<r-1&&r) {
       size_type q=(r-p)/2+p;
-      size_type r1=OrderStat(p,q,fc);
-      size_type r2=OrderStat(q,r,fc);
-      size_type r3=MergeStat(p,q,r,fc);
+      size_type r1=OrderStat(v,p,q,fc);
+      size_type r2=OrderStat(v,q,r,fc);
+      size_type r3=MergeStat(v,p,q,r,fc);
       return r1+r2+r3;
-    }
+    } else return 0;
   }
 
   template<typename T>
-  size_type MergeStat(size_type p,size_type q,size_type r,compare fc) {
+  size_type MergeStat(vector<T>&v,size_type p,size_type q,size_type r,compare fc) {
     vector<T> L(v.begin()+p,v.begin()+q);
     vector<T> R(v.begin()+q,v.begin()+r);
     size_type rp=0;
@@ -70,7 +70,7 @@ namespace stat {
       if (fc((void*)&L[i],(void*)&R[j])<0) v[k++]=L[i++];
       else {
         v[k++]=R[j++];
-        rp+=q-p-i;
+        rp+=L.size()-i;
       }
       if (i==q-p) {
         copy(R.begin()+j,R.end(),v.begin()+k);
@@ -85,13 +85,13 @@ namespace stat {
   }
 
   template<typename T>
-  size_type BruteForceStat(vector<T>&v,compare fc) {
-    size_type r=0;
-    for (size_type i=0;i<v.size();i++)
-      for (size_type j=i+1;j<v.size();j++)
+  size_type BruteForceStat(vector<T>&v,size_type p,size_type r,compare fc) {
+    size_type s=0;
+    for (size_type i=p;i<r;i++)
+      for (size_type j=i+1;j<r;j++)
         if (fc((void*)&v[j],(void*)&v[i])<0)
-          r++;
-    return r;
+          s++;
+    return s;
   }
 
   template<typename T>
---
+++ src/test/main.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+#include <cctype>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include "sorting.h"
+#include "orderstat.h"
+
+using namespace std;
+
+int comp(void*a,void*b) {
+  return *(int*)a-*(int*)b;
+}
+
+int main(void) {
+  int num[256]={0};
+  unsigned int n=sizeof(num)/sizeof(num[0]);
+  for (unsigned int i=0;i<n;i++) num[i]=i;
+  vector<int> v(num,num+n);
+  stat::Shuffle(v);
+
+  for (vector<int>::size_type i=0;i<v.size();i++)
+    cout<<' '<<v[i];
+  cout<<endl;
+
+  vector<int> o(v);
+  stat::size_type osr=stat::OrderStat(o,0,o.size(),comp);
+  cout<<osr<<endl;
+  vector<int> b(v);
+  osr=stat::BruteForceStat(b,0,b.size(),comp);
+  cout<<osr<<endl;
+  vector<int> vec(v);
+  sort::QuickSort(vec,0,vec.size(),comp);
+  for (sort::size_type i=0;i<vec.size();i++)
+    cout<<' '<<vec[i];
+  cout<<endl;
+  return 0;
+}
