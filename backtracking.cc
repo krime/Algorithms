4a5adfe3e2cf7ed35abde8f1e3c68df7c130390b
krime <krimelam@gmail.com>
2013-05-03 04:37:17 +0000

    Update Backtrack Algorithm

--- backtracking.cc
+++ backtracking.cc
@@ -13,10 +13,12 @@ void process_solution(int[],int);
 
 bool finished=false;
 
+// TODO: check whether the k-th trial satisfies the solution
 bool is_a_solution(int a[],int k,int n) {
-  return k+1==n;
+  return k==n-1;
 }
 
+// TODO: construct the probable solutions
 void construct_candidates(int a[],int k,int n,int c[],int&ncandidates) {
   c[0]=true;
   c[1]=false;
@@ -31,18 +33,21 @@ void process_solution(int a[],int k) {
   cout<<" }"<<endl;
 }
 
+// TODO: do the k-th backtrack
 void backtrack(int a[],int k,int input) {
   int c[MAXCANDIDATES]; // candidates for next positions
   int ncandidates;      // next position candidates count
-  k++;
 
+  // TODO: check the former solution vector a[0..k-1]
   if (is_a_solution(a,k-1,input))
     process_solution(a,k);
   else {
     construct_candidates(a,k,input,c,ncandidates);
     for (int i=0;i<ncandidates;i++) {
       a[k]=c[i];
+      // TODO: the next trial
       backtrack(a,k+1,input);
+      // TODO: for finding only 1 solution
       if (finished) return;
     }
   }
@@ -51,6 +56,7 @@ void backtrack(int a[],int k,int input) {
 int main(void) {
   int a[MAX];
   memset(a,0,sizeof a);
+  // TODO: start the first trial
   backtrack(a,0,MAX);
   return 0;
 }
