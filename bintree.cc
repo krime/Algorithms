79c6ded2b40080ddaf5d5623af6efe10a512f1bb
krime <krimelam@gmail.com>
2013-05-03 22:44:29 +0800

    Update Backtrack Algorithm

--- bintree.cc
+++ bintree.cc
@@ -2,13 +2,23 @@
 
 using namespace std;
 
+template <typename T>
 class Bintree {
-    private:
-        class Node {
-            Node*lt;
-            Node*rt;
-        };
-    public:
-        Node*root;
+protected:
+  struct Node {
+    Node*lt;
+    Node*rt;
+    T key;
+    Node(T k):key(k){}
+  };
+private:
+  Node*root;
+public:
+  Bintree():root(NULL){}
+  Bintree(T e);
+  Bintree(const Bintree&);
+  Bintree& operator=(Bintree&);
+  const Bintree& operator=(const Bintree&);
+  ~Bintree();
 };
 
